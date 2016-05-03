9b26824b0dfc09fac911eb3566b89733bcb5e9a5
zhufree <zhufree2013@gmail.com>
2016-05-03 19:54:58 +0800

    gulp test

---
+++ gulpfile.js
@@ -0,0 +1,30 @@
+var gulp = require('gulp'),
+	sass = require('gulp-sass'),
+	minifycss = require('gulp-minify-css'),
+	uglify = require('gulp-uglify'),
+	rename = require('gulp-rename'),
+	livereload = require('gulp-livereload'),
+    concat = require('gulp-concat');
+
+gulp.task('styles', function(){
+	gulp.src('src/css/**/*.scss')
+		.pipe(sass({outputStyle: 'expanded'})
+			.on('error', sass.logError))
+		.pipe(gulp.dest('dist/css'))
+		.pipe(rename({suffix: '.min'}))
+		.pipe(minifycss())
+		.pipe(gulp.dest('dist/css'));
+});
+
+gulp.task('scripts', function () {
+    gulp.src('src/js/*.js')
+        // .pipe(concat('main.js'))
+        .pipe(gulp.dest('dist/js'))
+        .pipe(rename({suffix: '.min'}))
+        .pipe(uglify())
+        .pipe(gulp.dest('dist/js'));
+});
+
+gulp.task('default', function() {
+	gulp.start('styles', 'scripts');
+});
