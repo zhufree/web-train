941b3609f2e4d2898d1efe6a6686ae45bb73a1b8
zhufree <zhufree2013@gmail.com>
2016-05-08 13:54:12 +0800

    woshipm copy  update

--- src/js/woshipm.js
+++ src/js/woshipm.js
@@ -2,8 +2,21 @@ $(function(){
 	$(document).on('mouseover', '#menu li,#user', function(event) {
 		event.preventDefault();
 		$(event.target).next('.dropdown-menu,#user-ctrl').show();
-	}).on('mouseout', '#menu li,#user', function(event) {
+	})
+	.on('mouseout', '#menu li,#user', function(event) {
 		event.preventDefault();
 		$(event.target).closest('.dropdown-menu,#user-ctrl').hide();
+	})
+	.on('click', '.dot', function(event) {
+		event.preventDefault();
+		$this = $(event.target);
+		$this.addClass('on');
+		$this.siblings().removeClass('on');
+		$("#recommend>ul:not('#ul-"+$this.attr('id').substr(-1)+"')").hide();
+		$("#recommend>ul#ul-"+$this.attr('id').substr(-1)).show();
+	})
+	.on('click', '#top', function(event) {
+		event.preventDefault();
+		$(window).scrollTop('0');
 	});
 });
